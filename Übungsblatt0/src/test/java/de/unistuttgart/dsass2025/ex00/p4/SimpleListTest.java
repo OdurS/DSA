73220479807c3202eab41ad39b72ee822f996df8
OdurS <odur.schweikert@gmail.com>
2025-04-19 17:25:39 +0200

    Übungsblatt0 hinzugefügt

---
+++ Übungsblatt0/src/test/java/de/unistuttgart/dsass2025/ex00/p4/SimpleListTest.java
@@ -0,0 +1,14 @@
+package de.unistuttgart.dsass2025.ex00.p4;
+
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+import java.util.concurrent.TimeUnit;
+
+public class SimpleListTest {
+
+}
