2bac2f868349c537ab26573e5546e0d83c0be6df
OdurS <odur.schweikert@gmail.com>
2025-04-19 19:25:51 +0200

    Übungsblatt1 Fertig?

--- Übungsblatt0/src/main/java/de/unistuttgart/dsass2025/ex00/p4/SimpleList.java
+++ Übungsblatt0/src/main/java/de/unistuttgart/dsass2025/ex00/p4/SimpleList.java
@@ -28,5 +28,116 @@ public class SimpleList<T extends Comparable<T>> implements ISimpleListIterable<
     public T getElement(int index) {
         return list.get(index);
     }
+    //Schweikert
+    private class SimpleListIterator implements Iterator<T> {
+        private int currentIndex = 0;
+        private int lastReturnedIndex = -1;
 
+        public SimpleListIterator(){
+		}
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex < list.size();
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            T element = list.get(currentIndex);
+            lastReturnedIndex = currentIndex;
+        
+            currentIndex++;
+            return element;
+        }
+
+        @Override
+        public void remove() {
+            if (lastReturnedIndex == -1) {
+                throw new IllegalStateException("next() must be called before remove()");
+            }
+            list.remove(lastReturnedIndex);
+            currentIndex--; // adjust for removed item
+            lastReturnedIndex = -1;
+        }
+        public void iterator(){
+            while(hasNext()){
+                next();
+
+            }
+        }
+    }
+
+
+
+
+
+    private class SimpleListSkippingIterator extends SimpleListIterator implements Iterator<T>  {
+        private int currentIndex = 0;
+        private int lastReturnedIndex = -1;
+        private int currentStepSize = 1;
+         
+       private SimpleListSkippingIterator(int stepSize) {
+        currentStepSize = stepSize;
+       }
+        
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex + currentStepSize < list.size();
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            T element = list.get(currentIndex);
+            lastReturnedIndex = currentIndex;
+            currentIndex = currentIndex + currentStepSize;
+            return element;
+        }
+
+        @Override
+        public void remove() {
+            if (lastReturnedIndex == -1) {
+                throw new IllegalStateException("next() must be called before remove()");
+            }
+            list.remove(lastReturnedIndex);
+            currentIndex--; 
+            lastReturnedIndex = -1;
+        }
+    }
+    
+    /**
+	 * Returns an iterator that enumerates every
+	 * element in the collection, starting with the
+	 * first element.
+	 *
+	 * @return the iterator.
+	 */
+	@Override
+	public Iterator<T> iterator() {
+		return new SimpleListIterator();
+	}
+
+
+
+
+	/**
+	 * Returns an iterator that enumerates every n-th
+	 * element in the collection, starting with the
+	 * first element.
+	 *
+	 * @param stepSize the number of skipped elements+1.
+	 * @return the iterator.
+	 * @throws IllegalArgumentException if stepSize is less than 1.
+	 */
+	@Override
+	public Iterator<T> skippingIterator(int stepSize) {
+		return new SimpleListSkippingIterator(stepSize);
+	}
 }
+
